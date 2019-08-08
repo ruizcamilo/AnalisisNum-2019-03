516b83f517b8cd04f8a3f04f39b6225842be6450
ruizcamilo <53006065+ruizcamilo@users.noreply.github.com>
2019-08-07 23:07:17 -0500

    Update punto13.R

--- Taller1/EjerciciosLibro/punto13.R
+++ Taller1/EjerciciosLibro/punto13.R
@@ -1,14 +1,14 @@
 #Una formula iterativa de convergencia cuadratica podría ser la raíz
 root <- function(value, n, x_0, err) {
-  x_k = x_0
+  x = x_0
   deltax_k = 1
   it = 0
   while (abs(deltax_k) > err){
-    deltax_k = ( ( value/(x_k**(n-1)) ) - x_k )/n
-    x_k = x_k + deltax_k
+    deltax = ( ( value/(x**(n-1)) ) - x )/n
+    x = x + deltax_k
     it = it + 1
   }
-  return (x_k)
+  return (x)
 }
 ##Prueba
 index = 4
